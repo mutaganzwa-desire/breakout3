7000517a6f2d239ab60c76a7b5e803f61f0a7805
mutaganzwa-desire <d.mutaganzw@alustudent.com>
2025-01-29 10:50:45 +0200

    Updates made

--- the_creator.sh
+++ the_creator.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-mkdir students_directory
-#Creates students_directory
-cd students_directory
-touch cohort1.txt cohort2.txt cohort3.txt
-echo "Files created in students_directory"
-echo "This is the updated script!"
+#Check if the directory already exists
+students_dir="students_directory"
+if [ ! -d "$students_dir" ]; then
+	echo "Creating $students_dir"
+	mkdir "$students_dir"
+else
+	echo "Directory already exists"
+fi
+#Automatically create the files
+auto=("cohort1.txt" "cohort2.txt" "cohort3.txt")
+for auto in "${auto[@]}"; do
+	file_path="$students_dir/$auto"
+if [ ! -f "$file_path" ]; then
+	touch "$file_path"
+else
+	echo "$file_path already exists"
+fi
+done
